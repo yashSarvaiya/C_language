6d4ff9769910d6041ad7e2e1410f08e82b264e21
yashSarvaiya <113958312+yashSarvaiya@users.noreply.github.com>
2022-09-21 07:51:44 +0530

    Add files via upload

---
+++ project-1/4.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("  - - - \n");
+	printf(" |     |\n" );
+	printf(" Y     |\n" );
+	printf(" A     |\n" );
+	printf(" S     |\n" );
+	printf(" H     |\n" );
+	printf(" |     |\n" );
+	printf("  - - - " );
+	getch();
+}
